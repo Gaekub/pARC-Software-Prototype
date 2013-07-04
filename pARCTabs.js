4fafdf2dbea26e904f135c5b763e1016c23729c3
Jacob <bacojtagel@hotmail.com>
2013-07-03 21:29:57 -0800

    Attempt to fix collapse

--- pARCTabs.js
+++ pARCTabs.js
@@ -64,33 +64,46 @@
       var hashPos = url.lastIndexOf ( '#' );
       return url.substring( hashPos + 1 );
     }
-    
-    function showPageElement(what)  
-    {  
-        var obj = typeof what == 'object'  
-            ? what : document.getElementById(what);  
-      
-        obj.style.display = 'block';  
-        return false;  
-    }  
-      
-    function hidePageElement(what)  
-    {  
-        var obj = typeof what == 'object'  
-            ? what : document.getElementById(what);  
-      
-        obj.style.display = 'none';  
-        return false;  
-    }  
-      
-    function togglePageElementVisibility(what)  
-    {  
-        var obj = typeof what == 'object'  
-            ? what : document.getElementById(what);  
-      
-        if (obj.style.display == 'none')  
-            obj.style.display = 'block';  
-        else  
-            obj.style.display = 'none';  
-        return false;  
-}  
+
+    function showPageElement(what)
+    {
+        var obj = typeof what == 'object'
+            ? what : document.getElementById(what);
+
+        obj.style.display = 'block';
+        return false;
+    }
+
+    function hidePageElement(what)
+    {
+        var obj = typeof what == 'object'
+            ? what : document.getElementById(what);
+
+        obj.style.display = 'none';
+        return false;
+    }
+
+    function togglePageElementVisibility(what)
+    {
+        var obj = typeof what == 'object'
+            ? what : document.getElementById(what);
+
+        if (obj.style.display == 'none')
+            obj.style.display = 'block';
+        else
+            obj.style.display = 'none';
+        return false;
+}
+
+
+function ShowHide(divId)
+{
+if(document.getElementById(divId).style.display == 'none')
+{
+document.getElementById(divId).style.display='block';
+}
+else
+{
+document.getElementById(divId).style.display = 'none';
+}
+}
